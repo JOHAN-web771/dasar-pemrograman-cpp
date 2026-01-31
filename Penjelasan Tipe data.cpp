1666f5dbb1c1e18639e2ba08f463a5dba3d28dbd
JOHAN HANGGARA H. <2500018119@webmail.uad.ac.id>
2026-01-31 20:13:17 +0700

    Add files via upload

---
+++ Penjelasan Tipe data.cpp
@@ -0,0 +1,9 @@
+//Tipe data pemrograman adalah klasifikasi atau peneslompokan jenis nilai yang dapat disimpan dan dapat dioperasikan oleh progaram dalam jenis tipedata tertentu.
+//tipe daat itu penting karena untuk membantu mencegah kesalahan tipe data,mencegah serangan keamanan,dan agar program/coplier dapat berjalan dengan baik.
+//tipe data yang umum digunakan adalah : int:utk menyimpan nilai bilangan bulat,contoh: int =5.
+//float:menyimpan bilangan desimal, contoh: float pi :3.14
+//double:menyimpan bilangan desimal yang lebih tinggi, cth: double pi:3.145678
+//char:menyimpan karakter, cth: char huruf:'a'
+//bool:menyimpan nilai kebenaran, true/false, cth: Bool isAdmin:true
+//string: menyimpan teks, cth: string nama: "hello world"
+
